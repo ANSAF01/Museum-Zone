662a0ed58119e65288dd4619f272d7ef27e44682
Mohammed Ansaf <106778984+ANSAF01@users.noreply.github.com>
2025-05-22 12:16:35 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",         // Scan index.html in the project root for Tailwind classes
+    "./public/**/*.js",     // Scan JavaScript files in the public folder if they dynamically add Tailwind classes
+    // "./public/**/*.html", // This line can be removed if index.html was the only HTML file in public/ that used Tailwind classes
+                            // Or kept if other HTML files exist in public/ that use Tailwind classes and need scanning
+    "./src/**/*.css",       // Good practice: Scan source CSS files (like input.css) for @apply or theme() usage
+  ],
+  theme: {
+    extend: {
+      // Your custom theme extensions
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'playfair': ['Playfair Display', 'serif'],
+      },
+      colors: {
+        // Example: if you want to map your custom colors to Tailwind names
+        // 'custom-green': '#2ECC71',
+        // 'custom-dark': '#1C2526',
+      },
+    },
+  },
+  plugins: [
+    // Any Tailwind plugins you might be using
+  ],
+}
